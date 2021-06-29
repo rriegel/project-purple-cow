a64afc4509999281272a74604951ae86b88b904d
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-06-29 16:04:08 -0400

    -- Tried to compile code with parcel for the first time ---- Ran into some errors and realized I'll have to try that another day -- Deleted Parcel dependencies and install webpack and babel

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,2 +1,18 @@
 import React from 'react';
 import $ from 'jquery';
+
+const App = () => {
+  return (
+    <>
+      <div>
+        {console.log('App.jsx is connected')}
+      </div>
+      <div>
+        Hello World
+      </div>
+    </>
+  )
+
+};
+
+export default App;
